72ba847497bb6486127b0b86430dc13ad7dd262b
Andy Kwong <andyk314@gmail.com>
2015-06-22 15:54:36 -0700

    setup new rails app with tdd testing.

---
+++ app/models/project.rb
@@ -0,0 +1,23 @@
+class Project
+  attr_accessor :tasks
+
+  def initialize
+    @tasks = []
+  end
+
+  def incomplete_tasks
+    tasks.reject(&:completed?)
+  end
+
+  def done?
+    incomplete_tasks.empty?
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    incomplete_tasks.sum(&:size)
+  end
+end
